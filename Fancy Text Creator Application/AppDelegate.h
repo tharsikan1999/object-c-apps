e153bebd551ac5e1666279bf068552a94aff977e
Sathasivam Tarsikan <tharsikanking@gmail.com>
2025-10-23 11:56:56 +0530

    Initial Commit

---
+++ Fancy Text Creator Application/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Fancy Text Creator Application
+//
+//  Created by Tharsikan Sathasivam on 2025-10-23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Fancy Text Creator Application/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Fancy Text Creator Application
+//
+//  Created by Tharsikan Sathasivam on 2025-10-23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
